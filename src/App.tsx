4f7729c453d5119aa3e3e9426d658ae8a77d7ada
AreSteven19 <arestevenacevedopereira@gmail.com>
2023-11-14 23:23:58 -0600

    primer commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css';
+import imagen_frutas from './frutas_modulo_producto_sinfondo.png';
+import logos from './logos.png';
+import flecha from './FLECHA_NOFONDO.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor">
+      <div className="izquierda">
+
+      <img id='logos' src={logos} alt="logo" />
+
+      <div className="botones">
+
+      <div className="boton">
+        <button className='botonp'>Agregar Producto</button>
+        <div className="triangulo"></div>
+      </div>
+      <div className="boton">
+      <button className='botonp'>Modificar Producto</button>
+        <div className="triangulo"></div>
+      </div>
+      <div className="boton">
+      <button className='botonp'>Buscar Producto</button>
+        <div className="triangulo"></div>
+      </div>
+      <div className="boton">
+      <button className='botonp'>Eliminar Producto</button>
+        <div className="triangulo"></div>
+      </div>
+
+<div>
+<img id='flecha' src={flecha} alt="logo" />
+</div>
+
+
+      </div>
+
+      </div>
+      
+      <div className="derecha">
+
+         <div className='fecha'>
+          <h1>12/20/2023</h1>
+       </div>
+
+      
+
+
+        <div className='titulo'>
+        <h1>Módulo </h1>
+        <h1 id='de'>de</h1>
+        <h1>producto</h1>
+        </div>
+
+        <br />
+
+        <img id='imagen_fruta' src={imagen_frutas} alt="Ejemplo" />
+        
+      </div>
+
+      
     </div>
   );
 }
